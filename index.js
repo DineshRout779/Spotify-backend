56d968acaec014150759cea566801b8a631d06db
Dinesh Rout <rddinesh779@gmail.com>
2024-01-06 00:13:49 +0530

    Fix: node-fetch errors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const app = require('express')();
-const { default: fetch } = require('node-fetch');
-const querystring = require('querystring');
-const cors = require('cors')
-require('dotenv').config();
+import express from 'express';
+import fetch from 'node-fetch';
+import querystring from 'querystring';
+import cors from 'cors';
+import dotnev from 'dotenv';
+
+dotnev.config();
+
+const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-	origin: 'https://dineshrout.netlify.app'
-}))
+app.use(cors());
 
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
@@ -20,75 +22,75 @@ const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
-	try {
-		const body = querystring.stringify({
-			grant_type: 'refresh_token',
-			refresh_token,
-		});
-		const headers = {
-			Authorization: `Basic ${basic}`,
-			'Content-Type': 'application/x-www-form-urlencoded',
-		};
+  try {
+    const body = querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token,
+    });
+    const headers = {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
 
-		const res = await fetch(TOKEN_ENDPOINT, {
-			method: 'POST',
-			headers: headers,
-			body: body,
-		});
+    const res = await fetch(TOKEN_ENDPOINT, {
+      method: 'POST',
+      headers: headers,
+      body: body,
+    });
 
-		return await res.json();
-	} catch (error) {
-		console.log(error);
-	}
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getNowPlaying = async (req, res) => {
-	const { access_token } = await getAccessToken();
+  const { access_token } = await getAccessToken();
 
-	return fetch(NOW_PLAYING_ENDPOINT, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	});
+  return fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
 };
 
 const getTopTracks = async (req, res) => {
-	const { access_token } = await getAccessToken();
+  const { access_token } = await getAccessToken();
 
-	return fetch(TOP_TRACKS_ENDPOINT, {
-		headers: {
-			Authorization: `Bearer ${access_token}`,
-		},
-	});
+  return fetch(TOP_TRACKS_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
 };
 
 app.get('/getCurrentTrack', async (req, res) => {
-	const response = await getNowPlaying();
+  const response = await getNowPlaying();
 
-	if (response.status === 204 || response.status > 400) {
-		return res.status(200).json({ isPlaying: false });
-	}
+  if (response.status === 204 || response.status > 400) {
+    return res.status(200).json({ isPlaying: false });
+  }
 
-	const song = await response.json();
-	const isPlaying = song.is_playing;
-	const title = song.item.name;
-	const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
-	const album = song.item.album.name;
-	const albumImageUrl = song.item.album.images[0].url;
-	const songUrl = song.item.external_urls.spotify;
+  const song = await response.json();
+  const isPlaying = song.is_playing;
+  const title = song.item.name;
+  const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+  const album = song.item.album.name;
+  const albumImageUrl = song.item.album.images[0].url;
+  const songUrl = song.item.external_urls.spotify;
 
-	return res.status(200).json({
-		song: {
-			album,
-			albumImageUrl,
-			artist,
-			songUrl,
-			title,
-		},
-		isPlaying: song.is_playing,
-	});
+  return res.status(200).json({
+    song: {
+      album,
+      albumImageUrl,
+      artist,
+      songUrl,
+      title,
+    },
+    isPlaying: song.is_playing,
+  });
 });
 
 app.listen(port, () => {
-	console.log(`server started on http://localhost:${port}`);
+  console.log(`server started on http://localhost:${port}`);
 });
